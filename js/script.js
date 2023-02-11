5160de72ecc4fb9164910abca87e17af94874f80
Maxim Polanov <maximpolanov.work@gmail.com>
2023-02-11 19:32:13 +0600

    part1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+alert(`Вы посмотрели ${numberOfFilms} фильмов`);
+
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privar: false
+};
+
+
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+	  rating1 = prompt('На сколько оцените его?', ''),
+	  lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+	  rating2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastFilm1] = rating1;
+personalMovieDB.movies[lastFilm2] = rating2;
+console.log(personalMovieDB);
