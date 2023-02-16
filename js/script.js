1ace2221a40d65ec97d9238d467de9a51e474d8c
Maxim Polanov <maximpolanov.work@gmail.com>
2023-02-16 15:24:52 +0600

    practicePart2

--- js/script.js
+++ js/script.js
@@ -13,12 +13,34 @@ const personalMovieDB = {
 	privar: false
 };
 
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Error');
+};
 
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-	  rating1 = prompt('На сколько оцените его?', ''),
-	  lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-	  rating2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilm1] = rating1;
-personalMovieDB.movies[lastFilm2] = rating2;
+const howManyFilms = 2;
+for (let i = 0; i < howManyFilms; i++) {
+	const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+	  	  rating = prompt('На сколько оцените его?', '');
+	if (lastFilm != null && rating != null && 50 >= lastFilm.length > 0 && 50 >= rating.length > 0) {
+		personalMovieDB.movies[lastFilm] = rating;
+		console.log('Done');
+	} else {
+		console.log('Error');
+		i--;
+	}
+	
+};
 console.log(personalMovieDB);
+
+/* if (lastFilm.length <= 0) {
+
+} else if (lastFilm.length > 50) {
+
+} */
